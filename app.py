cf089ef4d28d7fbd252958b965b51fc0d63bbddf
Brandon McFarlin <brandon.mcfarlin@outlook.com>
2021-10-04 23:45:33 -0500

    initial_commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+from subprocess import Popen
+import sys
+import time
+
+app = Flask(__name__)
+
+running_processes = []
+
+@app.route("/")
+def hello_world():
+    return "<p>Happy Halloween</p>"
+
+@app.route("/play_video", methods=['GET'])
+def play_video():
+    video_name = request.args.get('video_name', None)
+    if video_name == None:
+        return "invalid video name. try something like /play_video?video_name=video1"
+    process_id = Popen([sys.executable, 'playvideo.py', '--video_name', video_name])
+    running_processes.append(process_id)
+    print(process_id)
+    return "started playing the video..."
+
+@app.route("/stop_video")
+def stop_video():
+    print(running_processes)
+    killed_processes = []
+    while running_processes:
+        process_to_kill = running_processes.pop()
+        killed_processes.append(process_to_kill.args)
+        process_to_kill.kill()
+        if running_processes == []:
+            return f'killed processes {killed_processes}'
+    else:
+        return 'No active processes to terminate'
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
---
+++ playvideo.py
@@ -0,0 +1,47 @@
+import cv2
+from ffpyplayer.player import MediaPlayer
+import argparse
+
+video_dict = {
+    "video1": "C:\\example\\full\\path\\to\\video",
+    "video2": "source\video2.mp4"
+}
+
+
+
+def testing(video_name, video_dict):
+    video_path = video_dict.get(video_name, None)
+    if video_path == None:
+        raise ValueError('No path to video found')
+    video=cv2.VideoCapture(video_path)
+    player = MediaPlayer(video_path)
+    while True:
+        grabbed, frame=video.read()
+        player.get_frame()
+        if not grabbed:
+            print("End of video")
+            video.release()
+            break
+        ## Checks to see if the kill key has been pressed. In this case it is "q".
+        if cv2.waitKey(28) & 0xFF == ord("q"):
+            print('recieved kill command')
+            video.release()
+            player.close_player()
+            cv2.destroyAllWindows()
+            return('exiting successfully...')
+        # Actually display the image in a window
+        cv2.namedWindow("video", cv2.WND_PROP_FULLSCREEN)          
+        cv2.setWindowProperty("video", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        cv2.imshow("video", frame)
+    video.release()
+    player.close_player()
+
+    return testing()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Input File path...')
+    parser.add_argument('--video_name', help='name of the video mapped in video_dict', dest='video_name')
+
+    args = parser.parse_args()
+    print(args.video_name)
+    testing(args.video_name, video_dict)
